5e40c53a7191f0e800c810ee3801fa017b3be2cc
OleksiiKuzminov <Oleksii_Kuzminov@epam.com>
2018-10-22 16:57:17 +0300

    Add data import file upload endpoints

--- NEWS.md
+++ NEWS.md
@@ -1,2 +1,4 @@
+## 2018-10-22 v0.0.2
+ * Stub API endpoints was added
 ## 2018-09-24 v0.0.1
  * Initial module setup
--- src/test/java/org/folio/rest/RestVerticleTest.java
+++ src/test/java/org/folio/rest/RestVerticleTest.java
@@ -1,68 +1,153 @@
 package org.folio.rest;
 
 
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.builder.RequestSpecBuilder;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.specification.RequestSpecification;
 import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.http.HttpClientResponse;
-import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.apache.http.HttpStatus;
+import org.folio.rest.client.TenantClient;
+import org.folio.rest.persist.PostgresClient;
 import org.folio.rest.tools.utils.NetworkUtils;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.util.UUID;
 
 @RunWith(VertxUnitRunner.class)
 public class RestVerticleTest {
 
-  private Vertx vertx;
+  private static final String TENANT = "diku";
+  private static final String DEFINITION_PATH = "/data-import/upload/definition";
+  private static final String FILE_PATH = "/data-import/upload/file";
 
-  private int port;
+  private static Vertx vertx;
+  private static int port;
+  private static RequestSpecification spec;
+  private static RequestSpecification specUpload;
 
-  @Before
-  public void setUp(TestContext context) {
+  @BeforeClass
+  public static void setUpClass(final TestContext context) throws Exception {
+    Async async = context.async();
     vertx = Vertx.vertx();
     port = NetworkUtils.nextFreePort();
-    DeploymentOptions options = new DeploymentOptions()
-      .setConfig(new JsonObject().put("http.port", port));
-    vertx.deployVerticle(RestVerticle.class.getName(), options, context.asyncAssertSuccess());
-  }
 
-  @After
-  public void tearDown(TestContext context) {
-    vertx.close(context.asyncAssertSuccess());
+    String useExternalDatabase = System.getProperty(
+      "org.folio.password.validator.test.database",
+      "embedded");
+
+    switch (useExternalDatabase) {
+      case "environment":
+        System.out.println("Using environment settings");
+        break;
+      case "external":
+        String postgresConfigPath = System.getProperty(
+          "org.folio.data.import.test.config",
+          "/postgres-conf-local.json");
+        PostgresClient.setConfigFilePath(postgresConfigPath);
+        break;
+      case "embedded":
+        PostgresClient.setIsEmbedded(true);
+        PostgresClient.getInstance(vertx).startEmbeddedPostgres();
+        break;
+      default:
+        String message = "No understood database choice made." +
+          "Please set org.folio.data.import.test.database" +
+          "to 'external', 'environment' or 'embedded'";
+        throw new Exception(message);
+    }
+
+    TenantClient tenantClient = new TenantClient("localhost", port, "diku", "dummy-token");
+    DeploymentOptions restVerticleDeploymentOptions = new DeploymentOptions().setConfig(new JsonObject().put("http.port", port));
+    vertx.deployVerticle(RestVerticle.class.getName(), restVerticleDeploymentOptions, res -> {
+      try {
+        tenantClient.postTenant(null, res2 -> {
+          async.complete();
+        });
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    });
+
+    spec = new RequestSpecBuilder()
+      .setContentType(ContentType.JSON)
+      .setBaseUri("http://localhost:" + port)
+      .addHeader(RestVerticle.OKAPI_HEADER_TENANT, TENANT)
+      .build();
+
+    specUpload = new RequestSpecBuilder()
+      .setContentType("multipart/form-data")
+      .setBaseUri("http://localhost:" + port)
+      .addHeader(RestVerticle.OKAPI_HEADER_TENANT, TENANT)
+      .build();
   }
 
   @Test
-  public void testGetTemplateStub(TestContext context) {
-    //TODO Replace testing stub
-    final Async async = context.async();
-    String url = "http://localhost:" + port;
-    String templateTestUrl = url + "/data-import-ping";
-
-    Handler<HttpClientResponse> handler = response -> {
-      context.assertEquals(response.statusCode(), 200);
-      context.assertEquals(response.headers().get("content-type"), "application/json");
-      async.complete();
-    };
-    sendRequest(templateTestUrl, HttpMethod.GET, handler);
+  public void uploadDefinitionCreate() {
+    RestAssured.given()
+      .spec(spec)
+      .body(new JsonObject().toString())
+      .when()
+      .post(DEFINITION_PATH)
+      .then()
+      .statusCode(HttpStatus.SC_CREATED);
   }
 
-  private void sendRequest(String url, HttpMethod method, Handler<HttpClientResponse> handler) {
-    sendRequest(url, method, handler, "");
+  @Test
+  public void uploadDefinitionGet() {
+    RestAssured.given()
+      .spec(spec)
+      .when()
+      .get(DEFINITION_PATH)
+      .then()
+      .statusCode(HttpStatus.SC_OK);
   }
 
-  private void sendRequest(String url, HttpMethod method, Handler<HttpClientResponse> handler, String content) {
-    Buffer buffer = Buffer.buffer(content);
-    vertx.createHttpClient()
-      .requestAbs(method, url, handler)
-      .putHeader("x-okapi-tenant", "diku")
-      .putHeader("Accept", "application/json,text/plain")
-      .end(buffer);
+  @Test
+  public void uploadDefinitionGetById() {
+    RestAssured.given()
+      .spec(spec)
+      .when()
+      .get(DEFINITION_PATH + "/" + UUID.randomUUID().toString())
+      .then()
+      .statusCode(HttpStatus.SC_OK);
+  }
+
+  @Test
+  public void uploadDefinitionUpdate() {
+    RestAssured.given()
+      .spec(spec)
+      .when()
+      .get(DEFINITION_PATH + "/" + UUID.randomUUID().toString())
+      .then()
+      .statusCode(HttpStatus.SC_OK);
+  }
+
+  //@Test
+  public void fileUpload() {
+    RestAssured.given()
+      .spec(specUpload)
+      .when()
+      .post(FILE_PATH + "?jobExecutionId=" + UUID.randomUUID().toString() + "&uploadDefinitionId=" + UUID.randomUUID().toString())
+      .then()
+      .statusCode(HttpStatus.SC_CREATED);
+  }
+
+  @Test
+  public void fileDelete() {
+    RestAssured.given()
+      .spec(spec)
+      .when()
+      .delete(FILE_PATH + "/" + UUID.randomUUID().toString())
+      .then()
+      .statusCode(HttpStatus.SC_NO_CONTENT);
   }
 }

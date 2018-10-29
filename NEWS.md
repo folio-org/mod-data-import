60dd8fb07e0e568dcabf39bd87605ef8454fb12a
OleksiiKuzminov <Oleksii_Kuzminov@epam.com>
2018-10-29 14:45:54 +0200

    fix tests

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,5 @@
+## 2018-10-22 v0.0.3
+ * Services skeleton was added
 ## 2018-10-22 v0.0.2
  * Stub API endpoints was added
 ## 2018-09-24 v0.0.1
--- src/main/java/org/folio/service/FileStorageService.java
+++ src/main/java/org/folio/service/FileStorageService.java
@@ -1,57 +1,30 @@
 package org.folio.service;
 
 import io.vertx.core.Future;
-import org.folio.rest.jaxrs.model.File;
 
-import java.util.List;
-import java.util.Optional;
+import java.io.File;
 
 /**
- * File service
+ * File storage service. For each implementation should implement this service
  */
 
 public interface FileStorageService {
 
   /**
-   * Searches for Files
-   *
-   * @param query  CQL query
-   * @param offset offset
-   * @param limit  limit
-   * @return future with list of Files
+   * Search file at storage
    */
-  Future<List<File>> getFiles(String query, int offset, int limit);
+  Future<File> getFile(String path);
+
 
   /**
-   * Searches for File by id
-   *
-   * @param id File id
-   * @return future with optional File
+   * Saves File to the storage and return its path
    */
-  Future<Optional<File>> getFileById(String id);
+  Future<String> saveFile(File file);
+
 
   /**
-   * Saves File with generated id
-   *
-   * @param file File to save
-   * @return future with generated id
+   * Delete File
    */
-  Future<String> addFile(File file);
-
-  /**
-   * Updates File with given id
-   *
-   * @param file File to update
-   * @return future with true is succeeded
-   */
-  Future<Boolean> updateFile(File file);
-
-  /**
-   * Deletes File by id
-   *
-   * @param id File id
-   * @return future with true is succeeded
-   */
-  Future<Boolean> deleteFile(String id);
+  Future<Boolean> deleteFile(String path);
 
 }

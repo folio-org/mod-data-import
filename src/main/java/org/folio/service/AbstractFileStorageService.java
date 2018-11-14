653a2df8746d66bbdccbb905fb7cebe9b5b14a77
OleksiiKuzminov <Oleksii_Kuzminov@epam.com>
2018-11-14 11:02:11 +0200

    Change API and ModuleDescriptor

--- src/main/java/org/folio/service/AbstractFileStorageService.java
+++ src/main/java/org/folio/service/AbstractFileStorageService.java
@@ -2,19 +2,24 @@ package org.folio.service;
 
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.file.FileSystem;
+import org.apache.commons.io.IOUtils;
 import org.folio.rest.jaxrs.model.FileDefinition;
 
-import java.io.File;
+import javax.ws.rs.BadRequestException;
 import java.io.InputStream;
 
 public class AbstractFileStorageService implements FileStorageService {
 
   private Vertx vertx;
   private String tenantId;
+  private FileSystem fs;
 
   public AbstractFileStorageService(Vertx vertx, String tenantId) {
     this.vertx = vertx;
     this.tenantId = tenantId;
+    this.fs = vertx.fileSystem();
   }
 
   @Override
@@ -24,17 +29,40 @@ public class AbstractFileStorageService implements FileStorageService {
 
 
   @Override
-  public Future<File> getFile(String path) {
+  public Future<Buffer> getFile(String path) {
     return null;
   }
 
   @Override
-  public   Future<FileDefinition> saveFile(InputStream data, FileDefinition fileDefinition) {
-    return Future.succeededFuture(fileDefinition);
+  public Future<FileDefinition> saveFile(InputStream data, FileDefinition fileDefinition) {
+    Future<FileDefinition> future = Future.future();
+    getStoragePath(fileDefinition.getUploadDefinitionId())
+      .setHandler(pathReply -> {
+        if (pathReply.succeeded()) {
+          try {
+            String path = pathReply.result();
+            fs.mkdirsBlocking(path);
+            fs.writeFileBlocking(path + "/" + fileDefinition.getName(), Buffer.buffer(IOUtils.toByteArray(data)));
+            fileDefinition.setSourcePath(path + "/" + fileDefinition.getName());
+            fileDefinition.setLoaded(true);
+            future.complete(fileDefinition);
+          } catch (Exception e) {
+            future.fail(e);
+          }
+        } else {
+          future.fail(new BadRequestException());
+        }
+      });
+    return future;
+  }
+
+  public Future<String> getStoragePath(String uploadDefinitionId) {
+    return Future.succeededFuture("./storage/upload/" + uploadDefinitionId);
   }
 
   @Override
   public Future<Boolean> deleteFile(String path) {
     return null;
   }
+
 }
--- src/main/java/org/folio/service/FileStorageService.java
+++ src/main/java/org/folio/service/FileStorageService.java
@@ -1,9 +1,9 @@
 package org.folio.service;
 
 import io.vertx.core.Future;
+import io.vertx.core.buffer.Buffer;
 import org.folio.rest.jaxrs.model.FileDefinition;
 
-import java.io.File;
 import java.io.InputStream;
 
 /**
@@ -20,14 +20,14 @@ public interface FileStorageService {
   /**
    * Search file at storage
    */
-  Future<File> getFile(String path);
-
+  Future<Buffer> getFile(String path);
 
   /**
    * Saves File to the storage and return its path
    */
   Future<FileDefinition> saveFile(InputStream data, FileDefinition fileDefinition);
 
+  Future<String> getStoragePath(String uploadDefinitionId);
 
   /**
    * Delete File

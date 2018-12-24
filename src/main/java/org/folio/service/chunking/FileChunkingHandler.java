4708f77f6933e88f928b1396661561eff7c40738
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2018-12-24 14:24:33 +0200

    MODDATAIMP-39 - implementation for the FileChunkingHandle using sequential composition

---
+++ src/main/java/org/folio/service/chunking/FileChunkingHandler.java
@@ -0,0 +1,25 @@
+package org.folio.service.chunking;
+
+import io.vertx.core.Future;
+import org.folio.rest.jaxrs.model.Profile;
+import org.folio.rest.jaxrs.model.UploadDefinition;
+import org.folio.util.OkapiConnectionParams;
+
+/**
+ * Interface for the file chunking.
+ */
+public interface FileChunkingHandler {
+
+  /**
+   * Handles files linked to given UploadDefinition entity.
+   * Stating initiation of the file dividing process for given UploadDefinition entity.
+   * Divides all the files linked to one UploadDefinition into chunks of data
+   * and sends it the mod-source-record-manager.
+   *
+   * @param uploadDefinition UploadDefinition entity which files will be handled
+   * @param profile          JobProfile chosen by the user
+   * @param params           parameters necessary to connect to the OKAPI
+   * @return Future parametrized by handled {@link UploadDefinition}
+   */
+  Future<UploadDefinition> handle(UploadDefinition uploadDefinition, Profile profile, OkapiConnectionParams params);
+}

50c390ab8b14dc82f138f5099afc053559919d60
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2019-03-20 22:11:02 +0200

    * Moved InitFileProcessorAPI to rest.impl package

--- src/main/java/org/folio/rest/impl/InitFileProcessorAPI.java
+++ src/main/java/org/folio/rest/impl/InitFileProcessorAPI.java
@@ -1,11 +1,13 @@
-package org.folio.service.processing;
+package org.folio.rest.impl;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.serviceproxy.ServiceBinder;
 import org.folio.rest.resource.interfaces.InitAPI;
+import org.folio.service.processing.FileProcessor;
 
 /**
  * Performs components registration before the verticle is deployed.
@@ -16,5 +18,6 @@ public class InitFileProcessorAPI implements InitAPI {
     new ServiceBinder(vertx)
       .setAddress(FileProcessor.FILE_PROCESSOR_ADDRESS)
       .register(FileProcessor.class, FileProcessor.create(vertx));
+    handler.handle(Future.succeededFuture(true));
   }
 }

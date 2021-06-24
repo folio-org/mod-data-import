039f1f7640610171e29a36242c6c2d546bf9c5dc
Oleksandr <oleksandr_dekin@epam.com>
2021-06-24 15:33:42 +0300

    MODDATAIMP-464 Change dataType to have have common type for MARC related subtypes
    - changed data type from MARC to MARC_BIB
    - created migration script for removing MARC_AUTHORITY, MARC_HOLDING and changing MARC_BIB to MARC

---
+++ src/main/resources/templates/db_scripts/update_data_type_for_file_extensions.sql
@@ -0,0 +1,19 @@
+-- default_file_extensions:
+-- change values: from MARC_HOLDING to MARC, remove MARC_AUTHORITY and MARC_BIB
+UPDATE ${myuniversity}_${mymodule}.default_file_extensions
+SET jsonb = regexp_replace(jsonb::text, '\"MARC_BIB\",', '', 'g')::jsonb
+WHERE id IN ('5cf7a0e5-f359-43f5-8c96-e5df74131183','1df36889-4d3a-482e-a8c8-9c67b8245feb',
+             'f445092c-94b8-408a-a9f1-5edd8b5919c9','8ffe5abc-a532-48cb-ab0e-827ea980154c',
+             '6cf5568b-4d37-4d35-9116-e8a52120779d');
+
+UPDATE ${myuniversity}_${mymodule}.default_file_extensions
+SET jsonb = regexp_replace(jsonb::text, '\"MARC_AUTHORITY\",', '', 'g')::jsonb
+WHERE id IN ('5cf7a0e5-f359-43f5-8c96-e5df74131183','1df36889-4d3a-482e-a8c8-9c67b8245feb',
+             'f445092c-94b8-408a-a9f1-5edd8b5919c9','8ffe5abc-a532-48cb-ab0e-827ea980154c',
+             '6cf5568b-4d37-4d35-9116-e8a52120779d');
+
+UPDATE ${myuniversity}_${mymodule}.default_file_extensions
+SET jsonb = regexp_replace(jsonb::text, '\"MARC_HOLDING\"', '"MARC"', 'g')::jsonb
+WHERE id IN ('5cf7a0e5-f359-43f5-8c96-e5df74131183','1df36889-4d3a-482e-a8c8-9c67b8245feb',
+             'f445092c-94b8-408a-a9f1-5edd8b5919c9','8ffe5abc-a532-48cb-ab0e-827ea980154c',
+             '6cf5568b-4d37-4d35-9116-e8a52120779d');

8e397d1f4b00f872838b69904e3b07b60bd2c25d
Serhii Nosko <Serhii_Nosko@epam.com>
2021-07-29 13:36:23 +0300

    Add max request size

--- src/main/java/org/folio/config/ApplicationConfig.java
+++ src/main/java/org/folio/config/ApplicationConfig.java
@@ -27,6 +27,8 @@ public class ApplicationConfig {
   private String okapiUrl;
   @Value("${REPLICATION_FACTOR:1}")
   private int replicationFactor;
+  @Value("${MAX_REQUEST_SIZE:1048576}")
+  private int maxRequestSize;
   @Value("${ENV:folio}")
   private String envId;
 
@@ -41,6 +43,7 @@ public class ApplicationConfig {
       .kafkaPort(kafkaPort)
       .okapiUrl(okapiUrl)
       .replicationFactor(replicationFactor)
+      .maxRequestSize(maxRequestSize)
       .build();
     LOGGER.info("kafkaConfig: {}", kafkaConfig);
 

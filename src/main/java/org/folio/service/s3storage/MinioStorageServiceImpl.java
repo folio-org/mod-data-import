f4e222132d5bc24b90e9740f1f8a037ff17cbd2b
Noah Overcash <novercash@ebsco.com>
2023-06-13 13:10:51 -0500

    Add tests for MinioStorageService

--- src/main/java/org/folio/service/s3storage/MinioStorageServiceImpl.java
+++ src/main/java/org/folio/service/s3storage/MinioStorageServiceImpl.java
@@ -1,58 +1,73 @@
 package org.folio.service.s3storage;
 
+import io.minio.http.Method;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
-import lombok.SneakyThrows;
 import org.folio.rest.jaxrs.model.FileUploadInfo;
+import org.folio.s3.client.FolioS3Client;
+import org.folio.s3.exception.S3ClientException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MinioStorageServiceImpl implements MinioStorageService {
 
-  @Autowired
   private FolioS3ClientFactory folioS3ClientFactory;
 
-  @Autowired
   private Vertx vertx;
 
-  @Value("${minio.bucket}")
-  private String bucket;
-
-  @SneakyThrows
-  public Future<FileUploadInfo> getFileUploadUrl(String upLoadFileName, String tenantId) {
-    Promise<FileUploadInfo> promise = Promise.promise();
-    var client = folioS3ClientFactory.getFolioS3Client();
-    var key = buildKey(tenantId, upLoadFileName);
-
-    vertx.executeBlocking(blockingFuture -> {
-      String url = null;
-      try {
-        url = client.getPresignedUrl(key);
-      } catch (Exception e) {
-        blockingFuture.fail(e);
-      }
-      blockingFuture.complete(url);
-    }, asyncResult -> {
-      if (asyncResult.failed()) {
-        promise.fail(asyncResult.cause());
-      } else {
-        String url = (String) asyncResult.result();
-        FileUploadInfo fileUpload = new FileUploadInfo();
-        fileUpload.setUrl(url);
-        fileUpload.setKey(key);
-        promise.complete(fileUpload);
-      }
-    });
-
-    return promise.future();
-
+  @Autowired
+  public MinioStorageServiceImpl(
+    FolioS3ClientFactory folioS3ClientFactory,
+    Vertx vertx
+  ) {
+    this.folioS3ClientFactory = folioS3ClientFactory;
+    this.vertx = vertx;
   }
 
-  private String buildKey(String tenantId, String fileName) {
-    String format = String.format("%s/%d-%s", tenantId, System.currentTimeMillis(), fileName);
-    return format;
+  public Future<FileUploadInfo> getFileUploadUrl(
+    String uploadFileName,
+    String tenantId
+  ) {
+    Promise<FileUploadInfo> promise = Promise.promise();
+    FolioS3Client client = folioS3ClientFactory.getFolioS3Client();
+
+    String key = buildKey(tenantId, uploadFileName);
+
+    vertx.executeBlocking(
+      (Promise<String> blockingFuture) -> {
+        String url = null;
+        try {
+          url = client.getPresignedUrl(key, Method.PUT);
+        } catch (S3ClientException e) {
+          blockingFuture.fail(e);
+        }
+        blockingFuture.complete(url);
+      },
+      (AsyncResult<String> asyncResult) -> {
+        if (asyncResult.failed()) {
+          promise.fail(asyncResult.cause());
+        } else {
+          String url = (String) asyncResult.result();
+          FileUploadInfo fileUpload = new FileUploadInfo();
+          fileUpload.setUrl(url);
+          fileUpload.setKey(key);
+          promise.complete(fileUpload);
+        }
+      }
+    );
+
+    return promise.future();
+  }
+
+  private static String buildKey(String tenantId, String fileName) {
+    return String.format(
+      "%s/%d-%s",
+      tenantId,
+      System.currentTimeMillis(),
+      fileName
+    );
   }
 }
---
+++ src/test/java/org/folio/service/s3storage/MinioStorageServiceTest.java
@@ -0,0 +1,111 @@
+package org.folio.service.s3storage;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+
+import io.minio.http.Method;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.folio.rest.jaxrs.model.FileUploadInfo;
+import org.folio.s3.client.FolioS3Client;
+import org.folio.s3.exception.S3ClientException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(VertxUnitRunner.class)
+public class MinioStorageServiceTest {
+
+  private final Vertx vertx = Vertx.vertx();
+
+  @Mock
+  private FolioS3ClientFactory folioS3ClientFactory;
+
+  @Mock
+  private FolioS3Client folioS3Client;
+
+  private MinioStorageService minioStorageService;
+
+  @Before
+  public void setUp(TestContext context) {
+    vertx.exceptionHandler((err -> context.fail(err)));
+
+    MockitoAnnotations.openMocks(this);
+    this.minioStorageService =
+      new MinioStorageServiceImpl(folioS3ClientFactory, vertx);
+
+    Mockito
+      .when(folioS3ClientFactory.getFolioS3Client())
+      .thenReturn(folioS3Client);
+  }
+
+  @Test
+  public void testSuccessful(TestContext context) {
+    Async async = context.async();
+
+    Mockito
+      .when(folioS3Client.getPresignedUrl(anyString(), eq(Method.PUT)))
+      .thenReturn("url");
+
+    Future<FileUploadInfo> result = minioStorageService.getFileUploadUrl(
+      "test-file",
+      "test-tenant"
+    );
+
+    result.onFailure(_err -> context.fail("getFileUploadUrl should not fail"));
+    result.onSuccess(fileInfo -> {
+      Mockito.verify(folioS3ClientFactory, times(1)).getFolioS3Client();
+      Mockito
+        .verify(folioS3Client, times(1))
+        .getPresignedUrl(fileInfo.getKey(), Method.PUT);
+      Mockito.verifyNoMoreInteractions(folioS3ClientFactory);
+      Mockito.verifyNoMoreInteractions(folioS3Client);
+
+      assertEquals("Presigned URL is returned", "url", fileInfo.getUrl());
+      assertTrue(
+        "Key format is correct",
+        fileInfo.getKey().matches("test-tenant/\\d*-test-file")
+      );
+      async.complete();
+    });
+  }
+
+  @Test
+  public void testFailure(TestContext context) {
+    Async async = context.async();
+
+    S3ClientException exception = new S3ClientException("test exception");
+
+    Mockito
+      .when(folioS3Client.getPresignedUrl(anyString(), eq(Method.PUT)))
+      .thenThrow(exception);
+
+    Future<FileUploadInfo> result = minioStorageService.getFileUploadUrl(
+      "test-file",
+      "test-tenant"
+    );
+
+    result.onSuccess(_result -> context.fail("getFileUploadUrl should fail"));
+    result.onFailure(err -> {
+      Mockito.verify(folioS3ClientFactory, times(1)).getFolioS3Client();
+      Mockito
+        .verify(folioS3Client, times(1))
+        .getPresignedUrl(anyString(), eq(Method.PUT));
+      Mockito.verifyNoMoreInteractions(folioS3ClientFactory);
+      Mockito.verifyNoMoreInteractions(folioS3Client);
+
+      assertSame("Fails with getPresignedUrl exception", exception, err);
+      async.complete();
+    });
+  }
+}

f285ef07268412f4e748ebfde92d7dfc7c39303a
evaluk <evaluk@ebsco.com>
2023-07-18 16:09:27 -0400

    fix for mapping problems

--- src/main/java/org/folio/rest/impl/DataImportImpl.java
+++ src/main/java/org/folio/rest/impl/DataImportImpl.java
@@ -501,7 +501,7 @@ public class DataImportImpl implements DataImport {
           entity.getKey()
         );
         minioStorageService.completeMultipartFileUpload(entity.getKey(),  entity.getUploadId(),entity.getTags())
-          .map( PostDataImportAssembleStorageFileResponse::respond204)
+          .map(  deleted -> deleted ? PostDataImportAssembleStorageFileResponse.respond204() : PostDataImportAssembleStorageFileResponse.respond500WithTextPlain("Failed to assemble Data Import upload file") )
           .map(Response.class::cast)
           .otherwise(ExceptionHelper::mapExceptionToResponse)
           .onComplete(asyncResultHandler);

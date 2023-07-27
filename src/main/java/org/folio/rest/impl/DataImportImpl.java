9af3ac900057237c642e903fcd0009d4cf13c6ee
evaluk <evaluk@ebsco.com>
2023-07-27 10:25:37 -0400

    fix for code smell

--- src/main/java/org/folio/rest/impl/DataImportImpl.java
+++ src/main/java/org/folio/rest/impl/DataImportImpl.java
@@ -501,7 +501,7 @@ public class DataImportImpl implements DataImport {
           entity.getKey()
         );
         minioStorageService.completeMultipartFileUpload(entity.getKey(),  entity.getUploadId(),entity.getTags())
-          .map(  completed -> completed ? PostDataImportAssembleStorageFileResponse.respond204() : PostDataImportAssembleStorageFileResponse.respond400WithTextPlain("Failed to assemble Data Import upload file") )
+          .map(  completed -> Boolean.TRUE.equals(completed) ? PostDataImportAssembleStorageFileResponse.respond204() : PostDataImportAssembleStorageFileResponse.respond400WithTextPlain("Failed to assemble Data Import upload file") )
           .map(Response.class::cast)
           .otherwise(ExceptionHelper::mapExceptionToResponse)
           .onComplete(asyncResultHandler);

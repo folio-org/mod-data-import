c4bbd9b884abaf3091ed39cada2e7263997ea05e
Noah Overcash <novercash@ebsco.com>
2023-08-24 19:30:16 -0400

    Include random ID for new queue items

--- src/main/java/org/folio/service/file/SplitFileProcessingService.java
+++ src/main/java/org/folio/service/file/SplitFileProcessingService.java
@@ -7,8 +7,6 @@ import io.vertx.core.Vertx;
 import io.vertx.ext.web.handler.HttpException;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -16,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.Data;
 import org.apache.logging.log4j.LogManager;
@@ -240,6 +239,7 @@ public class SplitFileProcessingService {
             queueItemDao
               .addQueueItem(
                 new DataImportQueueItem()
+                  .withId(UUID.randomUUID().toString())
                   .withJobExecutionId(execution.getId())
                   .withUploadDefinitionId(parentUploadDefinition.getId())
                   .withTenant(tenant)

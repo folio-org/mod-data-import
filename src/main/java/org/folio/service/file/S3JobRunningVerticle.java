c9c36a81946702c1ff5bdbe15905228e129aa0a7
Noah Overcash <novercash@ebsco.com>
2023-08-28 21:33:05 -0400

    Add S3 split chunk job runner

---
+++ src/main/java/org/folio/service/file/S3JobRunningVerticle.java
@@ -0,0 +1,276 @@
+package org.folio.service.file;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
+import java.util.Map;
+import java.util.concurrent.Semaphore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.folio.dataimport.util.OkapiConnectionParams;
+import org.folio.kafka.KafkaConfig;
+import org.folio.rest.jaxrs.model.DataImportQueueItem;
+import org.folio.rest.jaxrs.model.JobExecution;
+import org.folio.rest.jaxrs.model.StatusDto;
+import org.folio.rest.jaxrs.model.StatusDto.ErrorStatus;
+import org.folio.service.auth.SystemUserAuthService;
+import org.folio.service.processing.ParallelFileChunkingProcessor;
+import org.folio.service.processing.ranking.ScoreService;
+import org.folio.service.s3storage.MinioStorageService;
+import org.folio.service.upload.UploadDefinitionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Worker verticle to handle running jobs from S3 storage.
+ *
+ * This is configured as a verticle to enable
+ */
+@Component
+public class S3JobRunningVerticle extends AbstractVerticle {
+
+  private static final int MS_BETWEEN_IDLE_POLLS = 5000;
+
+  private static final Logger LOGGER = LogManager.getLogger();
+
+  private MinioStorageService minioStorageService;
+  private ScoreService scoreService;
+  private SystemUserAuthService systemUserService;
+  private UploadDefinitionService uploadDefinitionService;
+
+  private ParallelFileChunkingProcessor fileProcessor;
+
+  // used to allow wake-ups from other threads and the timer
+  private final Semaphore semaphore;
+
+  @Autowired
+  public S3JobRunningVerticle(
+    Vertx vertx,
+    MinioStorageService minioStorageService,
+    ScoreService scoreService,
+    SystemUserAuthService systemUserService,
+    UploadDefinitionService uploadDefinitionService,
+    KafkaConfig kafkaConfig
+  ) {
+    LOGGER.info("Constructing S3JobRunningVerticle");
+
+    this.vertx = vertx;
+
+    this.minioStorageService = minioStorageService;
+    this.systemUserService = systemUserService;
+    this.scoreService = scoreService;
+    this.uploadDefinitionService = uploadDefinitionService;
+
+    this.fileProcessor = new ParallelFileChunkingProcessor(vertx, kafkaConfig);
+
+    this.semaphore = new Semaphore(1);
+  }
+
+  @Override
+  public void start() {
+    LOGGER.info("Running S3JobRunningVerticle");
+
+    this.run();
+  }
+
+  public void run() {
+    semaphore.release();
+    this.pollForJobs();
+  }
+
+  /**
+   * Loops indefinitely, polling for available jobs.
+   * This is the best approach, as the only other option is to use a trigger in the DB, which also requires polling.
+   * The semaphore is used to allow us to wake this loop up on an interval and when a job is sent to this worker.
+   */
+  private synchronized void pollForJobs() {
+    try {
+      semaphore.acquire();
+
+      // we want to drain all permits so that only one is available at a time
+      // e.g. if we're manually invoked and the timer ends, we don't want to loop extra
+      semaphore.drainPermits();
+
+      this.scoreService.getBestQueueItemAndMarkInProgress()
+        .compose(optional -> {
+          // a promise with result of if a queue item was processed or not
+          // this determines cool down before next check
+          Promise<Boolean> promise = Promise.promise();
+
+          if (optional.isEmpty()) {
+            promise.complete(false);
+          } else {
+            processQueueItem(optional.get())
+              .map(v -> true)
+              .onComplete(promise::handle);
+          }
+
+          return promise.future();
+        })
+        .onSuccess(didProcess -> {
+          if (Boolean.TRUE.equals(didProcess)) {
+            this.run();
+          } else {
+            // wait before checking again
+            LOGGER.info(
+              "No queue items available to run, checking again in {}ms",
+              MS_BETWEEN_IDLE_POLLS
+            );
+            vertx.setTimer(MS_BETWEEN_IDLE_POLLS, v -> this.run());
+          }
+        })
+        .onFailure(err -> {
+          LOGGER.error("Error running queue item...", err);
+          // immediately retry
+          this.run();
+        });
+    } catch (InterruptedException e) {
+      LOGGER.fatal("Interrupted while waiting for semaphore", e);
+      Thread.currentThread().interrupt();
+    }
+  }
+
+  protected Future<QueueJob> processQueueItem(DataImportQueueItem queueItem) {
+    LOGGER.info(
+      "Starting to process job execution {}",
+      queueItem.getJobExecutionId()
+    );
+
+    OkapiConnectionParams params = getConnectionParams(
+      queueItem.getTenant(),
+      queueItem.getOkapiUrl()
+    );
+
+    File localFile;
+    try {
+      localFile =
+        File.createTempFile("data-import-dl", queueItem.getJobExecutionId());
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+
+    return uploadDefinitionService
+      .getJobExecutionById(queueItem.getJobExecutionId(), params)
+      .map(jobExecution -> new QueueJob().withJobExecution(jobExecution))
+      .compose(job ->
+        uploadDefinitionService
+          .updateJobExecutionStatus(
+            job.getJobExecution().getId(),
+            new StatusDto().withStatus(StatusDto.Status.PROCESSING_IN_PROGRESS),
+            params
+          )
+          .map(successful -> {
+            if (Boolean.FALSE.equals(successful)) {
+              throw new IllegalStateException(
+                "Unable to mark job as in progress"
+              );
+            }
+            return job;
+          })
+      )
+      .compose(job ->
+        minioStorageService
+          .readFile(job.getJobExecution().getFileName())
+          .map(inputStream -> job.withInputStream(inputStream))
+      )
+      .map(job -> {
+        try {
+          OutputStream outputStream = new FileOutputStream(job.getFile());
+
+          job.getInputStream().transferTo(outputStream);
+
+          job.getInputStream().close();
+          outputStream.close();
+
+          return job;
+        } catch (IOException e) {
+          throw new UncheckedIOException(e);
+        }
+      })
+      .map(job -> {
+        fileProcessor.processFile(
+          job.getFile(),
+          job.getJobExecution().getId(),
+          job.getJobExecution().getJobProfileInfo(),
+          params
+        );
+        return job;
+      })
+      .onComplete(v -> {
+        try {
+          FileUtils.delete(localFile);
+        } catch (IOException e) {
+          LOGGER.error(
+            "Could not clean up temporary file {}: ",
+            localFile.toPath(),
+            e
+          );
+        }
+      })
+      .onFailure(v ->
+        uploadDefinitionService.updateJobExecutionStatus(
+          queueItem.getJobExecutionId(),
+          new StatusDto()
+            .withErrorStatus(ErrorStatus.FILE_PROCESSING_ERROR)
+            .withStatus(StatusDto.Status.ERROR),
+          params
+        )
+      );
+  }
+
+  /**
+   * Authenticate and get connection parameters (Okapi URL/token)
+   */
+  private OkapiConnectionParams getConnectionParams(
+    String tenant,
+    String okapiUrl
+  ) {
+    OkapiConnectionParams provisionalParams = new OkapiConnectionParams(
+      Map.of("x-okapi-url", okapiUrl, "x-okapi-tenant", tenant),
+      vertx
+    );
+
+    String token = systemUserService.getAuthToken(provisionalParams);
+
+    return new OkapiConnectionParams(
+      Map.of(
+        "x-okapi-url",
+        okapiUrl,
+        "x-okapi-tenant",
+        tenant,
+        "x-okapi-token",
+        token
+      ),
+      vertx
+    );
+  }
+
+  @Override
+  public void stop() {
+    LOGGER.info("Stopping S3JobRunningVerticle");
+  }
+
+  @Data
+  @With
+  @NoArgsConstructor
+  @AllArgsConstructor
+  private static class QueueJob {
+
+    private DataImportQueueItem queueItem;
+    private JobExecution jobExecution;
+    private InputStream inputStream;
+    private File file;
+  }
+}

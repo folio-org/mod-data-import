0b049fb459c5d64ea10ec12843766663dbe24f7f
Noah Overcash <novercash@ebsco.com>
2023-09-01 12:36:57 -0500

    Update assemble tests

--- src/test/java/org/folio/rest/DataImportAssembleFileTest.java
+++ src/test/java/org/folio/rest/DataImportAssembleFileTest.java
@@ -7,16 +7,20 @@ import static com.github.tomakehurst.wiremock.client.WireMock.put;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.fail;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import io.restassured.RestAssured;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.apache.http.HttpStatus;
 import org.folio.rest.jaxrs.model.AssembleFileDto;
 import org.folio.rest.jaxrs.model.FileDefinition;
@@ -44,6 +48,8 @@ public class DataImportAssembleFileTest extends AbstractRestTest {
 
   @Test
   public void testAssemble(TestContext context) {
+    UploadDefinition definition = createUploadDefinition();
+
     FileUploadInfo firstPartUploadInfo = getFirstPart("test-name1");
     FileUploadInfo secondPartUploadInfo = getLaterPart(
       firstPartUploadInfo.getKey(),
@@ -61,24 +67,10 @@ public class DataImportAssembleFileTest extends AbstractRestTest {
     tags.add(upload(secondPartUploadInfo.getUrl(), 5 * 1024 * 1024));
     tags.add(upload(thirdPartUploadInfo.getUrl(), 3 * 1024 * 1024));
 
-    System.out.println(tags.stream().collect(Collectors.joining(", ")));
-
-    UploadDefinition definition = createUploadDefinition();
-
     AssembleFileDto dto = new AssembleFileDto()
       .withKey(firstPartUploadInfo.getKey())
       .withUploadId(firstPartUploadInfo.getUploadId())
       .withTags(tags);
-
-    WireMock.stubFor(
-      put(
-        urlPathMatching(
-          "/change-manager/jobExecutions/" + JOB_EXEC_ID + "/status"
-        )
-      )
-        .withRequestBody(matchingJsonPath("$.status", equalTo("FILE_UPLOADED")))
-        .willReturn(okJson(JsonObject.mapFrom(new JobExecution()).toString()))
-    );
 
     RestAssured
       .given()
@@ -106,59 +98,55 @@ public class DataImportAssembleFileTest extends AbstractRestTest {
     );
   }
 
-  // @Test
-  // public void shouldFailAssembleFileFailedPartUpload(TestContext context) {
-  //   JsonPath info1 = RestAssured
-  //     .given()
-  //     .spec(spec)
-  //     .when()
-  //     .queryParam("fileName", "test-name1")
-  //     .get(UPLOAD_URL_PATH)
-  //     .jsonPath();
-  //   String uploadId1 = info1.get("uploadId");
-  //   String key1 = info1.get("key");
+  @Test
+  public void shouldFailAssembleFileFailedPartUpload(TestContext context) {
+    UploadDefinition definition = createUploadDefinition();
 
-  //   String url1 = info1.get("url");
-  //   ArrayList<String> tags = putFakeFile(context, url1, 1 * 1024 * 1024);
+    FileUploadInfo firstPartUploadInfo = getFirstPart("test-name1");
 
-  //   //upload 2nd piece
-  //   JsonPath info2 = RestAssured
-  //     .given()
-  //     .spec(spec)
-  //     .when()
-  //     .queryParam("key", key1)
-  //     .queryParam("uploadId", uploadId1)
-  //     .queryParam("partNumber", "2")
-  //     .get(UPLOAD_URL_CONTINUE_PATH)
-  //     .then()
-  //     .statusCode(HttpStatus.SC_OK)
-  //     .log()
-  //     .all()
-  //     .extract()
-  //     .body()
-  //     .jsonPath();
+    // no parts actually uploaded
+    AssembleFileDto dto = new AssembleFileDto()
+      .withKey(firstPartUploadInfo.getKey())
+      .withUploadId(firstPartUploadInfo.getUploadId())
+      .withTags(Arrays.asList("invalid"));
 
-  //   String url2 = info2.get("url");
+    RestAssured
+      .given()
+      .spec(spec)
+      .body(dto)
+      .pathParam("uploadDefinitionId", definition.getId())
+      .pathParam(
+        "fileDefinitionId",
+        definition.getFileDefinitions().get(0).getId()
+      )
+      .when()
+      .post(ASSEMBLE_PATH)
+      .then()
+      .statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
 
-  //   tags.addAll(putFakeFile(context, url2, 5 * 1024 * 1024));
+    UploadDefinition result = getUploadDefinition(definition.getId());
 
-  //   AssembleFileDto dto = new AssembleFileDto();
-  //   dto.setUploadId(uploadId1);
-  //   dto.setKey(key1);
-  //   dto.setTags(tags);
-  //   RestAssured
-  //     .given()
-  //     .spec(spec)
-  //     .body(dto, ObjectMapperType.GSON)
-  //     .when()
-  //     .post(ASSEMBLE_PATH)
-  //     .then()
-  //     .log()
-  //     .all()
-  //     .statusCode(HttpStatus.SC_BAD_REQUEST);
-  // }
+    assertThat(
+      result.getFileDefinitions().get(0).getSourcePath(),
+      is(nullValue())
+    );
+    assertThat(
+      result.getFileDefinitions().get(0).getStatus(),
+      is(FileDefinition.Status.UPLOADING)
+    );
+  }
 
   private UploadDefinition createUploadDefinition() {
+    WireMock.stubFor(
+      put(
+        urlPathMatching(
+          "/change-manager/jobExecutions/" + JOB_EXEC_ID + "/status"
+        )
+      )
+        .withRequestBody(matchingJsonPath("$.status", equalTo("FILE_UPLOADED")))
+        .willReturn(okJson(JsonObject.mapFrom(new JobExecution()).toString()))
+    );
+
     return RestAssured
       .given()
       .spec(spec)
@@ -184,16 +172,19 @@ public class DataImportAssembleFileTest extends AbstractRestTest {
   }
 
   private UploadDefinition getUploadDefinition(String id) {
-    return RestAssured
-      .given()
-      .spec(spec)
-      .when()
-      .get(DEFINITION_PATH + "?query=id==" + id)
-      .then()
-      .statusCode(HttpStatus.SC_OK)
-      .body("totalRecords", is(1))
-      .extract()
-      .path("uploadDefinitions[0]");
+    return JsonObject
+      .mapFrom(
+        RestAssured
+          .given()
+          .spec(spec)
+          .when()
+          .get(DEFINITION_PATH + "/" + id)
+          .then()
+          .statusCode(HttpStatus.SC_OK)
+          .extract()
+          .as(UploadDefinition.class)
+      )
+      .mapTo(UploadDefinition.class);
   }
 
   private FileUploadInfo getFirstPart(String filename) {
@@ -203,7 +194,9 @@ public class DataImportAssembleFileTest extends AbstractRestTest {
       .when()
       .queryParam("fileName", filename)
       .get(UPLOAD_URL_PATH)
-      .thenReturn()
+      .then()
+      .statusCode(HttpStatus.SC_OK)
+      .extract()
       .body()
       .as(FileUploadInfo.class);
   }
@@ -221,24 +214,25 @@ public class DataImportAssembleFileTest extends AbstractRestTest {
       .queryParam("uploadId", uploadId)
       .queryParam("partNumber", Integer.toString(partNumber))
       .get(UPLOAD_URL_CONTINUE_PATH)
-      .thenReturn()
+      .then()
+      .statusCode(HttpStatus.SC_OK)
+      .extract()
       .body()
       .as(FileUploadInfo.class);
   }
 
   private String upload(String url, int size) {
-    return RestAssured
-      .given()
-      .spec(spec)
-      .body(new byte[size])
-      .when()
-      .post(url)
-      .then()
-      .log()
-      .all()
-      .statusCode(200)
-      .extract()
-      .headers()
-      .toString();
+    // unsure how to make this work with restassured...
+    try {
+      HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+      con.setRequestMethod("PUT");
+      con.setDoOutput(true);
+      OutputStream output = con.getOutputStream();
+      output.write(new byte[size]);
+      return (con.getHeaderField("eTag"));
+    } catch (Exception e) {
+      fail(e.getMessage());
+      throw new IllegalStateException();
+    }
   }
 }

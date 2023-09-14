2f9ccaa526565b85af96950a803bc801b691eb11
Noah Overcash <novercash@ebsco.com>
2023-09-14 12:47:44 -0400

    Improve DownloadUrlAPITest

--- src/test/java/org/folio/rest/DownloadUrlAPITest.java
+++ src/test/java/org/folio/rest/DownloadUrlAPITest.java
@@ -30,7 +30,37 @@ public class DownloadUrlAPITest extends AbstractRestTest {
         .willReturn(
           okJson(
             JsonObject
-              .mapFrom(new JobExecution().withSourcePath("test-key-response"))
+              .mapFrom(
+                new JobExecution()
+                  .withSourcePath("data-import/test-key-response")
+              )
+              .toString()
+          )
+        )
+    );
+
+    RestAssured
+      .given()
+      .spec(spec)
+      .when()
+      .pathParam("jobExecutionId", JOB_EXEC_ID)
+      .get(DOWNLOAD_URL_PATH)
+      .then()
+      .statusCode(HttpStatus.SC_OK)
+      .body("url", containsString("test-bucket/test-key-response"));
+  }
+
+  @Test
+  public void testOutOfScopeRequest() {
+    WireMock.stubFor(
+      get("/change-manager/jobExecutions/" + JOB_EXEC_ID)
+        .willReturn(
+          okJson(
+            JsonObject
+              .mapFrom(
+                new JobExecution()
+                  .withSourcePath("not-correct-prefix/test-key-response")
+              )
               .toString()
           )
         )

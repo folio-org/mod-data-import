4f83b8c7747489c42c682541a12b27f192a1cd41
Noah Overcash <novercash@ebsco.com>
2023-10-27 14:26:34 -0400

    Add okapi permissions property/column

--- src/main/java/org/folio/service/file/SplitFileProcessingService.java
+++ src/main/java/org/folio/service/file/SplitFileProcessingService.java
@@ -288,6 +288,20 @@ public class SplitFileProcessingService {
                         entity.getJobProfileInfo().getDataType().toString()
                       )
                       .withOkapiToken(params.getToken())
+                      .withOkapiPermissions(
+                        params
+                          .getHeaders()
+                          .entries()
+                          .stream()
+                          .filter(header ->
+                            "x-okapi-permissions".equalsIgnoreCase(
+                                header.getKey()
+                              )
+                          )
+                          .findFirst()
+                          .map(Map.Entry::getValue)
+                          .orElse("")
+                      )
                   );
                 })
                 .map(Future.class::cast)
